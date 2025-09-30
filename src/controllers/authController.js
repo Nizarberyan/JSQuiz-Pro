056f7ce15c6c5b1345605d49e711ef9c8b732e52
ikrambenallali <ikrambenallali2003@gmail.com>
2025-09-30 16:19:17 +0100

    register

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,5 @@
-const bcrypt = require('bcrypt');  //bcrypt est une bibliothèque Node.js utilisée pour hachage et vérification sécurisée des mots de passe
-const User = require('../models/User');
+const bcrypt = require('bcryptjs');  //bcrypt est une bibliothèque Node.js utilisée pour hachage et vérification sécurisée des mots de passe
+const User = require('../../models');
 
 const register = async (req, res) => {
   try {
@@ -36,8 +36,7 @@ const register = async (req, res) => {
 };
 
 module.exports = { register };
-const bcrypt = require('bcryptjs');
-const { User } = require('../../models');
+
 
 
 
@@ -45,34 +44,34 @@ const showLogin = (req, res) => res.render('auth/login');
 const showRegister = (req, res) => res.render('auth/register');
 
 const login = async (req, res) => {
-    try {
-        const { username, password } = req.body;
-        const user = await User.findOne({ where: { name: username } });
-        if (!user) {
-            const hash = await bcrypt.hash('Nizar2005nizar', 10);
-            console.log(hash);
-            return res.render('auth/login', { error: 'Invalid credentials' });
-        }
-        const isMatch = await bcrypt.compare(password, user.password);
-        if (!isMatch) {
-            return res.render('auth/login', { error: 'Invalid credentials' });
-        }
-        req.session.userId = user.id;
-        req.session.userRole = user.role;
-        req.session.userPassword = user.password;
-
-        res.json({ 
-            message: 'Login successful',
-            session: {
-                userId: req.session.userId,
-                userRole: req.session.userRole,
-                userPassword: req.session.userPassword
-            }
-        });
-    } catch (err) {
-        console.error(err);
-        res.render('auth/login', { error: 'Something went wrong' });
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ where: { name: username } });
+    if (!user) {
+      const hash = await bcrypt.hash('Nizar2005nizar', 10);
+      console.log(hash);
+      return res.render('auth/login', { error: 'Invalid credentials' });
     }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.render('auth/login', { error: 'Invalid credentials' });
+    }
+    req.session.userId = user.id;
+    req.session.userRole = user.role;
+    req.session.userPassword = user.password;
+
+    res.json({
+      message: 'Login successful',
+      session: {
+        userId: req.session.userId,
+        userRole: req.session.userRole,
+        userPassword: req.session.userPassword
+      }
+    });
+  } catch (err) {
+    console.error(err);
+    res.render('auth/login', { error: 'Something went wrong' });
+  }
 }
 
 module.exports = { showLogin, showRegister, login };
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -4,6 +4,6 @@ const authController = require('../controllers/authController');
 
 router.get('/login', authController.showLogin);
 router.post('/login', authController.login);
-router.get('/register', authController.showRegister);
+router.post('/register', authController.register);
 
 module.exports = router;
