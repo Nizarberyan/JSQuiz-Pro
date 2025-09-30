892b0789ab0fcdb638db5690907248625f7580be
Nizar Beriane <nizarberiane@proton.me>
2025-09-30 10:25:22 +0100

    refactor db.js to use ES module syntax for dotenv and Sequelize imports

--- config/db.js
+++ config/db.js
@@ -1,6 +1,5 @@
-require('dotenv').config();
-
-const { Sequelize } = require('sequelize');
+import 'dotenv/config';
+import { Sequelize } from 'sequelize';
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
@@ -13,3 +12,5 @@ try {
 } catch (error) {
     console.error('Unable to connect to the database:', error);
 }
+
+export default sequelize;
