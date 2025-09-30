2709c97fd925b8c3f7e5a5c1a076a148410f601f
Nizar Beriane <nizarberiane@proton.me>
2025-09-30 12:16:46 +0100

    add Question and Score models with migrations; establish associations with User model

---
+++ migrations/20250930110437-create-question.js
@@ -0,0 +1,43 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable('Questions', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            theme: {
+                type: Sequelize.STRING,
+                allowNull: false
+            },
+            question: {
+                type: Sequelize.STRING,
+                allowNull: false
+            },
+            correctAnswer: {
+                type: Sequelize.JSON,
+                allowNull: false
+            },
+            options: {
+                type: Sequelize.JSON,
+                allowNull: false
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+                defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+                defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+            }
+        });
+    },
+    async down(queryInterface) {
+        await queryInterface.dropTable('Questions');
+    }
+};
