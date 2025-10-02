7048340621702be6127ab514b1324b15b75c31b0
ikrambenallali <ikrambenallali2003@gmail.com>
2025-10-02 10:24:55 +0100

    calcule score

--- models/user.js
+++ models/user.js
@@ -42,6 +42,7 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'User',
+    tableName: 'Users',
   });
   return User;
 };
--- src/routes/scores.js
+++ src/routes/scores.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const scoreController = require('../controllers/scoreController');
 
+// Middleware to ensure the user is authenticated
+// const authMiddleware = require('../middleware/authMiddleware');
+// router.use(authMiddleware.ensureAuth);
+
+router.post('/calculate', scoreController.calculateScore);
 
 
 module.exports = router;
